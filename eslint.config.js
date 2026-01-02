ff8f0fbcfe53540de19dd1a544f68efec49fcdfc
imcharliesparks <sparksfullstack@gmail.com>
2026-01-02 12:45:06 -0500

    updated lint config

--- eslint.config.js
+++ eslint.config.js
@@ -1,94 +1,99 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const js_1 = __importDefault(require("@eslint/js"));
-const typescript_eslint_1 = __importDefault(require("typescript-eslint"));
-const eslint_plugin_1 = __importDefault(require("@stylistic/eslint-plugin"));
-const eslint_plugin_n_1 = __importDefault(require("eslint-plugin-n"));
-const config_1 = require("eslint/config");
-exports.default = (0, config_1.defineConfig)(js_1.default.configs.recommended, eslint_plugin_n_1.default.configs['flat/recommended-script'], ...typescript_eslint_1.default.configs.strictTypeChecked, ...typescript_eslint_1.default.configs.stylisticTypeChecked, {
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+import stylistic from '@stylistic/eslint-plugin';
+import nodePlugin from 'eslint-plugin-n';
+
+export default [
+  eslint.configs.recommended,
+  nodePlugin.configs['flat/recommended-script'],
+  ...tseslint.configs.strictTypeChecked,
+  ...tseslint.configs.stylisticTypeChecked,
+  {
     ignores: [
-        '**/node_modules/*',
-        '**/*.mjs',
-        '**/*.js',
+      '**/node_modules/*',
+      '**/*.mjs',
+      '**/*.js',
     ],
-}, {
+  },
+  {
     languageOptions: {
-        parserOptions: {
-            project: './tsconfig.json',
-            warnOnUnsupportedTypeScriptVersion: false,
-        },
+      parserOptions: {
+        project: './tsconfig.json',
+        warnOnUnsupportedTypeScriptVersion: false,
+      },
     },
-}, {
+  },
+  {
     plugins: {
-        '@stylistic': eslint_plugin_1.default,
+      '@stylistic': stylistic,
     },
-}, {
+  },
+  {
     files: ['**/*.ts'],
-}, {
+  },
+  {
     rules: {
-        '@typescript-eslint/explicit-member-accessibility': 'warn',
-        '@typescript-eslint/no-misused-promises': 0,
-        '@typescript-eslint/no-floating-promises': 0,
-        '@typescript-eslint/no-confusing-void-expression': 0,
-        '@typescript-eslint/no-unnecessary-condition': 0,
-        '@typescript-eslint/restrict-template-expressions': [
-            'error', { allowNumber: true },
-        ],
-        '@typescript-eslint/restrict-plus-operands': [
-            'warn', { allowNumberAndString: true },
-        ],
-        '@typescript-eslint/no-unused-vars': [
-            'warn',
-            {
-                argsIgnorePattern: '^_',
-                varsIgnorePattern: '^_',
+      '@typescript-eslint/explicit-member-accessibility': 'warn',
+      '@typescript-eslint/no-misused-promises': 0,
+      '@typescript-eslint/no-floating-promises': 0,
+      '@typescript-eslint/no-confusing-void-expression': 0,
+      '@typescript-eslint/no-unnecessary-condition': 0,
+      '@typescript-eslint/restrict-template-expressions': [
+        'error', { allowNumber: true },
+      ],
+      '@typescript-eslint/restrict-plus-operands': [
+        'warn', { allowNumberAndString: true },
+      ],
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+        },
+      ],
+      '@typescript-eslint/no-unsafe-enum-comparison': 0,
+      '@typescript-eslint/no-unnecessary-type-parameters': 0,
+      '@stylistic/no-extra-semi': 'warn',
+      'max-len': [
+        'warn',
+        {
+          code: 80,
+        },
+      ],
+      '@stylistic/semi': ['warn', 'always'],
+      '@stylistic/member-delimiter-style': ['warn', {
+        multiline: {
+          delimiter: 'comma',
+          requireLast: true,
+        },
+        singleline: {
+          delimiter: 'comma',
+          requireLast: false,
+        },
+        overrides: {
+          interface: {
+            singleline: {
+              delimiter: 'semi',
+              requireLast: false,
             },
-        ],
-        '@typescript-eslint/no-unsafe-enum-comparison': 0,
-        '@typescript-eslint/no-unnecessary-type-parameters': 0,
-        '@stylistic/no-extra-semi': 'warn',
-        'max-len': [
-            'warn',
-            {
-                'code': 80,
+            multiline: {
+              delimiter: 'semi',
+              requireLast: true,
             },
-        ],
-        '@stylistic/semi': ['warn', 'always'],
-        '@stylistic/member-delimiter-style': ['warn', {
-                'multiline': {
-                    'delimiter': 'comma',
-                    'requireLast': true,
-                },
-                'singleline': {
-                    'delimiter': 'comma',
-                    'requireLast': false,
-                },
-                'overrides': {
-                    'interface': {
-                        'singleline': {
-                            'delimiter': 'semi',
-                            'requireLast': false,
-                        },
-                        'multiline': {
-                            'delimiter': 'semi',
-                            'requireLast': true,
-                        },
-                    },
-                },
-            }],
-        '@typescript-eslint/no-non-null-assertion': 0,
-        '@typescript-eslint/no-unused-expressions': 'warn',
-        'comma-dangle': ['warn', 'always-multiline'],
-        'no-console': 1,
-        'no-extra-boolean-cast': 0,
-        'indent': ['warn', 2],
-        'quotes': ['warn', 'single'],
-        'n/no-process-env': 1,
-        'n/no-missing-import': 0,
-        'n/no-unpublished-import': 0,
-        'prefer-const': 'warn',
+          },
+        },
+      }],
+      '@typescript-eslint/no-non-null-assertion': 0,
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      'comma-dangle': ['warn', 'always-multiline'],
+      'no-console': 1,
+      'no-extra-boolean-cast': 0,
+      indent: ['warn', 2],
+      quotes: ['warn', 'single'],
+      'n/no-process-env': 1,
+      'n/no-missing-import': 0,
+      'n/no-unpublished-import': 0,
+      'prefer-const': 'warn',
     },
-});
+  },
+];
